1bb448b1cddb6126952a0fd4a6b07a87cc64941c
Ashish Vishwakarma <ashish521523@gmail.com>
2026-06-11 16:40:05 +0530

    Updated plant

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,12 @@ import "./App.css";
 import Card from "./Component/Card";
 import Header from "./Component/Header";
 import Home from "./Component/Home";
+import Trendy_plant from "./Component/Trendy_plant";
 function App() {
   return (
     <div className="AppContenar">
       <Header></Header>
+      <Trendy_plant></Trendy_plant>
       <Home></Home>
       <Card></Card>
     </div>
